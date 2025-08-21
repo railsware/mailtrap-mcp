f58f9c8a061f327bd5fc650d1cce50cd85c68501
Narek Hovhannisyan <narhovhannisian@gmail.com>
2025-08-21 13:37:47 +0400

    Add sandbox client instance to Mailtrap configuration for enhanced email testing

--- src/client.ts
+++ src/client.ts
@@ -10,10 +10,29 @@ if (!MAILTRAP_API_TOKEN) {
 const client = new MailtrapClient({
   token: MAILTRAP_API_TOKEN,
   // conditionally set accountId if it's a valid number
-  ...(process.env.MAILTRAP_ACCOUNT_ID && !isNaN(Number(process.env.MAILTRAP_ACCOUNT_ID))
+  ...(process.env.MAILTRAP_ACCOUNT_ID &&
+  !Number.isNaN(Number(process.env.MAILTRAP_ACCOUNT_ID))
     ? { accountId: Number(process.env.MAILTRAP_ACCOUNT_ID) }
     : {}),
 });
 
+// Create a sandbox client instance
+const { MAILTRAP_TEST_INBOX_ID } = process.env;
+
+const sandboxClient =
+  MAILTRAP_TEST_INBOX_ID &&
+  !Number.isNaN(Number(process.env.MAILTRAP_TEST_INBOX_ID))
+    ? new MailtrapClient({
+        token: MAILTRAP_API_TOKEN!,
+        testInboxId: Number(process.env.MAILTRAP_TEST_INBOX_ID),
+        sandbox: true,
+        // conditionally set accountId if it's a valid number
+        ...(process.env.MAILTRAP_ACCOUNT_ID &&
+        !Number.isNaN(Number(process.env.MAILTRAP_ACCOUNT_ID))
+          ? { accountId: Number(process.env.MAILTRAP_ACCOUNT_ID) }
+          : {}),
+      })
+    : null;
+
 // eslint-disable-next-line import/prefer-default-export
-export { client };
+export { client, sandboxClient };
